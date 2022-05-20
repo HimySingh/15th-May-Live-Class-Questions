f5d4a0a50cb192f58fdaff27e1b34e94de850971
Himanshu Singh <96868684+HimySingh@users.noreply.github.com>
2022-05-20 12:46:05 +0530

    Add files via upload

---
+++ 15th May Live Class Questions.py
@@ -0,0 +1,90 @@
+"""
+# 1 . Try to extract data from index one to index 300 with a jump of 3
+n6= "this is My First Python programming class and i am learNING python string and its function"
+print(n6[0:300:3])
+"""
+
+"""
+# 2. Try to reverse a string without using reverse function 
+n6= "this is My First Python programming class and i am learNING python string and its function"
+print(n6[ : : -1])
+"""
+
+"""
+# 3. Try to split a string after conversion of entire string in uppercase 
+n6= "this is My First Python programming class and i am learNING python string and its function"
+print(n6.upper())
+print(len(n6))
+print(n6[2:90:2])
+print(n6[90:30:-2])
+print(n6[-6:-80:-3])
+"""
+
+"""
+# 4. Try to convert the whole string into lower case.
+n6= "this is My First Python programming class and i am learNING python string and its function"
+print(n6.lower())
+"""
+
+"""
+# 5 . Try to capitalize the whole string.
+n6= "this is My First Python programming class and i am learNING python string and its function"
+print(n6.capitalize())
+"""
+
+
+"""
+# 6 . Write a difference between isalnum() and isalpha().
+isalnum(): Returns TRUE when the characters in " " or ' ' are the mixture of ALPHABETS AND NUMBERS, WITHOUT SYMBOLS.
+isalpha(): Returns TRUE when the characters in " " or ‘ ’ are ALPHABET.  
+"""
+
+"""
+# 7. Try to give an example of expand tab.
+n7= "Him\tan\tshu"
+print(n7.expandtabs(2))
+"""
+
+"""
+# 8 . Give an example of strip , lstrip and rstrip.
+n8= "    Ramanuj    "
+print(n8.lstrip())
+print(n8.rstrip())
+print("The boy named", n8.strip(), "have a cycle.")
+"""
+
+"""
+# 9.  Replace a string character by another character by taking your own example.
+n6= "this is My First Python programming class and i am learNING python string and its function"
+print(n6.replace("this","@THAT@"))
+print(n6.replace("a", "$"))
+"""
+
+"""
+# 10 . Try to give a definition of string center function with and example.
+n9= "him"
+print(n9.center(16,"&"))
+The string center function put the string in the center of the space given in the first attribute of the function and fills the rest of the space with the character/symbol given in the 2nd attribute.
+"""
+
+"""
+# 11 . Write your own definition of compiler and interpreter without copy paste form internet in your own language.
+Compiler: It first scans/processes the entire source code and converts the code into machine language (0,1). This ML code is then executed by the processor. If there is an error in any line of the source code, the result will not be shown or performed by the processor.
+Interpreter: It reads the source code line by line and if there is an error in a line, it stops the further scanning/processing at that point and shows that error.  
+"""
+
+"""
+# 12 . Python is a interpreted or compiled language give a clear an with your understanding.
+Python is an interpreted language. Its code is translated into the machine language (0 & 1) during the execution-only.
+"""
+
+"""
+# 13 . Try to write use cases of python with your understanding.
+1. Face Detection 
+2. Vehicle Number Plate Detection 
+3. Stock Exchange Trading Apps that have AI for trading analysis 
+4. Phone Keyboard
+5. Snapchat's Bitmoji predictor
+6. Google Map 
+7. Airlines ticket price predicter
+"""
